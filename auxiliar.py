147a0918a2fe83f32c6dba569513fb1796b25f12
rafasousa <140452345+rafasousa1@users.noreply.github.com>
2024-07-08 23:04:58 -0300

    Initial commit

---
+++ auxiliar.py
@@ -0,0 +1,10 @@
+import pyautogui
+import time
+
+
+#descobrir em qual posição da tela clicar
+#pyautogui.position() mostra a posição do mouse
+#o mouse se mvimenta por eixo X para horizontal e eixo Y para vertical
+
+time.sleep(4)
+print(pyautogui.position())
---
+++ codigo.py
@@ -0,0 +1,48 @@
+import pyautogui
+import time
+import pandas as pd
+
+tabela = pd.read_csv("produtos.csv")
+
+pyautogui.PAUSE = 0.5
+
+pyautogui.press("win")
+pyautogui.write("edge")
+pyautogui.press("enter")
+pyautogui.write("https://dlp.hashtagtreinamentos.com/python/intensivao/login")
+pyautogui.press("enter")
+time.sleep(3)
+pyautogui.click(x=842, y=360)
+pyautogui.write("teste@gmail.com")
+pyautogui.press("tab")
+pyautogui.write("senhateste")
+pyautogui.press("tab")
+pyautogui.press("enter")
+
+for linha in tabela.index:
+    pyautogui.click(x=818, y=240)
+    pyautogui.write(str(tabela.loc[linha, "codigo"]))
+    pyautogui.press("tab")  
+
+    pyautogui.write(str(tabela.loc[linha, "marca"]))
+    pyautogui.press("tab")
+    
+    pyautogui.write(str(tabela.loc[linha, "tipo"]))
+    pyautogui.press("tab")
+    
+    pyautogui.write(str(tabela.loc[linha, "categoria"]))
+    pyautogui.press("tab")
+    
+    pyautogui.write(str(tabela.loc[linha, "preco_unitario"]))
+    pyautogui.press("tab")
+    
+    pyautogui.write(str(tabela.loc[linha, "custo"]))
+    pyautogui.press("tab")
+    
+    if not pd.isna(tabela.loc[linha, "obs"]):
+        pyautogui.write(str(tabela.loc[linha, "obs"]))
+    pyautogui.press("tab")
+    pyautogui.press("enter")
+    pyautogui.scroll(-1000)
+    time.sleep(1.5)
+    pyautogui.scroll(1000)
